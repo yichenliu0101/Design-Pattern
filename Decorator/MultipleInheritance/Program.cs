1c5b6500bf6662b24582a9f20e9bca0ee30c0350
Liu <aaronliu890101@gmail.com>
2025-02-27 12:52:56 +0800

    Adapter, Bridge, Composite, Decorator, Flyweight

---
+++ Decorator/MultipleInheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDesignPatternDemos.Structural.Decorator
+{
+    public class Bird
+    {
+        public void Fly()
+        {
+
+        }
+    }
+
+    public class Lizard
+    {
+        public void Crawl()
+        {
+
+        }
+    }
+
+    public class Dragon // no multiple inheritance
+    {
+        private Bird bird;
+        private Lizard lizard;
+
+        public Dragon(Bird bird, Lizard lizard)
+        {
+            this.bird = bird ?? throw new ArgumentNullException(paramName: nameof(bird));
+            this.lizard = lizard ?? throw new ArgumentNullException(paramName: nameof(lizard));
+        }
+
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+    }
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
